6ed226dbbcdec60b8c2ce6a6af6da50e14aaf82c
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2015-05-29 19:32:26 -0300

    Controller macro set api version

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,6 +2,7 @@ module ControllerMacros
   def headers
     before(:each) do
       @request.env["HTTP_ACCEPT"] = 'application/json'
+      @request.env["Accept"] = 'application/vnd.ucandidate.v1'
     end
   end
 end
