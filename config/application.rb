bfdc253690304879d3487fa5452c09d997041c2d
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2015-05-30 02:10:25 -0300

    Access

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,10 @@ module UcandidateApi
     end
 
     config.i18n.default_locale = "pt-BR"
+
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
   end
 end
