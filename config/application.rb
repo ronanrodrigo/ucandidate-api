fdf639f3f89832a3b78c938ecda87d471a8224a3
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2015-05-30 02:13:50 -0300

    Gh-page access

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module UcandidateApi
     config.i18n.default_locale = "pt-BR"
 
     config.action_dispatch.default_headers.merge!({
-      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Origin' => 'http://ronanrodrigo.github.io',
       'Access-Control-Request-Method' => '*'
     })
   end
