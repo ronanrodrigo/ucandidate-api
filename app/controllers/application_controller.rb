511a3638fafbdb36c71f43267d927a61537dae5d
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2015-05-30 02:09:15 -0300

    Access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   layout nil
   protect_from_forgery with: :exception
   respond_to :json
+  before_filter :set_access
+
+  def set_access
+    @response.headers["Access-Control-Allow-Origin"] = "*"
+  end
 end
